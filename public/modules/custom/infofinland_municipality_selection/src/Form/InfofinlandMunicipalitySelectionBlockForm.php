ca2b539147782d68ed8c7bc16b1894b03faa8d99
y4AxwNSeWVXrJPqTHxcN <neea.planting@phz.fi>
2021-09-16 21:39:28 +0300

    IFU-333: Fixed injections

--- public/modules/custom/infofinland_municipality_selection/src/Form/InfofinlandMunicipalitySelectionBlockForm.php
+++ public/modules/custom/infofinland_municipality_selection/src/Form/InfofinlandMunicipalitySelectionBlockForm.php
@@ -27,7 +27,6 @@ class InfofinlandMunicipalitySelectionBlockForm extends FormBase {
     return $instance;
   }
 
-
   /**
    * {@inheritdoc}
    */
--- public/modules/custom/infofinland_municipality_selection/src/Plugin/Block/InfofinlandMunicipalitySelectionBlock.php
+++ public/modules/custom/infofinland_municipality_selection/src/Plugin/Block/InfofinlandMunicipalitySelectionBlock.php
@@ -3,7 +3,10 @@
 namespace Drupal\infofinland_municipality_selection\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormBuilderInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a Infofinland Municipality Selection block which allows user to select their municipality
@@ -14,7 +17,30 @@ use Drupal\Core\Form\FormStateInterface;
  * )
  */
 
-class InfofinlandMunicipalitySelectionBlock extends BlockBase {
+class InfofinlandMunicipalitySelectionBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The form builder service.
+   *
+   * @var FormBuilderInterface
+   */
+  protected $formBuilder;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $form_builder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $form_builder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    // Instantiate this block class.
+    return new static($configuration, $plugin_id, $plugin_definition,
+      // Load the service required to construct this class.
+      $container->get('form_builder')
+    );
+  }
 
   /**
    * {@inheritdoc}
