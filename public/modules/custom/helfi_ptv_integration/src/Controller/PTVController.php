66194c3245a900e610ea9d829f6f36c18560717d
y4AxwNSeWVXrJPqTHxcN <neea.planting@phz.fi>
2021-10-29 16:41:22 +0300

    IFU-354: Added pv files

---
+++ public/modules/custom/helfi_ptv_integration/src/Controller/PTVController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\helfi_ptv_integration\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Connection;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class PTVController extends ControllerBase{
+
+  /**
+   * @var Connection
+   */
+  private Connection $connection;
+
+  public function __construct(Connection $connection)
+  {
+    $this->connection = $connection;
+  }
+
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('database')
+    );
+  }
+
+  public function showCodes()
+  {
+    $query = $this->connection->select('key_value', 'kv');
+    $query->addField('kv', 'name');
+    $query->addField('kv', 'value', 'code');
+    $query->condition('name', '%ptv_city%', 'LIKE');
+    $idData = $query->execute()->fetchAll();
+    $result = [];
+    foreach ($idData as $data) {
+      $result[] = ['name' => substr($data->name, 9), 'id' => unserialize($data->code)];
+    }
+    $header = [
+      'name' => 'Kunta',
+      'id' => 'ID'
+    ];
+    $form['table'] = [
+      '#type' => 'table',
+      '#header' => $header,
+      '#rows' => $result,
+      '#empty' => $this->t('No municipalities found'),
+    ];
+    return $form;
+  }
+}
