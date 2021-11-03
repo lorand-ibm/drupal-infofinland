b4493be6fd3972e84b843c0a73ff0563c320186b
Neea <neea.planting@hotmail.com>
2021-11-03 09:43:51 +0200

    FU-354: IFU-354 all the codes (#12)
    * IFU-354: IFU-354 all the codes, still wip
    
    * IFU-354: Added date thing
    
    * IFU-354: FIxed PR
    
    * IFU-354: Change to get organizations and added ptv code
    
    * IFU-354: Added organizations as a name
    
    * IFU-354: Added menu item and list
    
    * IFU-354: Added cron and view
    
    * IFU-354: Added pv files
    
    * IFU-354: Added more filters to view
    
    * IFU-354: Fixed cron date
    
    * IFU-354: Fixed some pr comments
    
    * IFU-354: Put automated cron on
    
    * IFU-354: Fixed spelling errors
    
    * IFU-354: Paragraph things
    
    * IFU-354: Paragraph settings
    
    * IFU-354: Changed automated updates and view text
    
    Co-authored-by: y4AxwNSeWVXrJPqTHxcN <neea.planting@phz.fi>
    Co-authored-by: Jussiles <juss.leskinen@gmail.com>

---
+++ public/modules/custom/helfi_ptv_integration/src/Commands/PTVDrushCommands.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\helfi_ptv_integration\Commands;
+
+use Drupal\helfi_ptv_integration\HelfiPTV;
+use Drush\Commands\DrushCommands;
+
+/**
+ * A Drush command file.
+ */
+class PTVDrushCommands extends DrushCommands {
+
+  /***
+   * @var HelfiPTV
+   */
+  protected HelfiPTV $ptv;
+
+  public function __construct(HelfiPTV $ptv) {
+    $this->ptv = $ptv;
+    parent::__construct();
+  }
+
+  /**
+   * Drush command that migrates office data.
+   * @param string $date
+   * @param string $city
+   *   Argument with the date to be used
+   * @command ptv_migrate_offices_custom_commands:ptv_migrate_offices
+   * @aliases drush-ptv_migrate_offices ptv_migrate_offices
+   * @usage ptv_migrate_custom_commands: ptv_migrate_offices date
+   */
+  public function ptv_migrate_offices($city = 'all', $date = '1970-01-01') {
+    $migrate = $this->ptv->getOfficeIdsPerCity($city, $date);
+    $this->output()->writeln('Migration finished for date ' . $date . ' and city ' . $city);
+  }
+
+  /**
+   * Drush command that migrates city codes
+   *   Argument with the date to be used
+   * @command ptv_migrate_custom_commands:ptv_migrate_cities
+   * @aliases drush-ptv_migrate_cities ptv_migrate
+   * @usage ptv_migrate_cities_custom_commands: ptv_migrate_cities
+   */
+  public function ptv_migrate_cities() {
+    $migrate = $this->ptv->getTheCityCodes();
+    $this->output()->writeln('Migration finished');
+  }
+}
