dc84596b5339c2945cc93c2c29da0dbfaa001842
Jussiles <juss.leskinen@gmail.com>
2022-03-10 13:41:52 +0200

    Add elastic settings.

--- public/sites/default/settings.php
+++ public/sites/default/settings.php
@@ -96,6 +96,17 @@ if ($env = getenv('APP_ENV')) {
   }
 }
 
+if (getenv('ELASTIC_URL')) {
+  $config['elasticsearch_connector.cluster.infofinland']['url'] = getenv('ELASTIC_URL');
+
+  if (getenv('ELASTIC_USER') && getenv('ELASTIC_PASSWORD')) {
+    $config['elasticsearch_connector.cluster.infofinland']['options']['use_authentication'] = '1';
+    $config['elasticsearch_connector.cluster.infofinland']['options']['authentication_type'] = 'Basic';
+    $config['elasticsearch_connector.cluster.infofinland']['options']['username'] = getenv('ELASTIC_USER');
+    $config['elasticsearch_connector.cluster.infofinland']['options']['password'] = getenv('ELASTIC_PASSWORD');
+  }
+}
+
 if ($varnish_host = getenv('DRUPAL_VARNISH_HOST')) {
   $config['varnish_purger.settings.default']['hostname'] = $varnish_host;
   $config['varnish_purger.settings.varnish_purge_all']['hostname'] = $varnish_host;
